4c58c00f1972ff8a97840b26e2a2493820d35ac8
kostiantynnm <kostiantyn.naftulovych@iisviolamarchesini.edu.it>
2023-09-19 18:43:43 +0200

    upload1

---
+++ Car_Plates/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Plates
+{
+    internal class Program //Naftulovych Kostiantyn
+    {
+        static void Main()
+        {
+            Console.Write("Enter an Italian car plate number (example: AA111CC ): ");
+            string plateInput = Console.ReadLine();
+
+                    // Check if the entered string has the correct format
+            if (IsValidCarNumber(plateInput))
+            {
+                        // Format the car number
+                string Plate = FormatCarNumber(plateInput);
+                //Console.WriteLine("Formatted number: " + Plate);
+
+                        // Convert the formatted number into an array of integers
+                int[] PlateInt = ConvertToNumber(Plate);
+
+                        // Calculate the position of the car number
+                Console.WriteLine("Position: " + Multiplication(PlateInt));
+            }
+            else
+            {
+                Console.WriteLine("Entered number does not match the Italian car plate format (example: AA111CC ).");
+            }
+            Console.ReadLine();
+        }
+
+                // Check if the input string has a valid Italian car plate format
+        static bool IsValidCarNumber(string input)
+        {
+            if (input.Length != 7)
+                return false;
+
+            for (int i = 0; i < 7; i++)
+            {
+                if ((i < 2 || (i > 4 && i < 7)) && !char.IsLetter(input[i]))
+                {
+                    return false;
+                }
+                else if ((i == 2 || i == 3 || i == 4) && !char.IsDigit(input[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+                // Format the car number by rearranging letters and digits
+        static string FormatCarNumber(string input)
+        {
+            string Number = input.Substring(0, 2) + input.Substring(5, 2) + input.Substring(2, 3);
+            return Number;
+        }
+
+                // Convert letters and digits in the formatted number into integers
+        static int[] ConvertToNumber(string input)
+        {
+            int[] output = new int[input.Length];
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                char letter = input[i];
+                if (Char.IsLetter(letter))
+                {
+                    int position = Char.ToUpper(letter) - 'A';
+                    output[i] = position;
+                }
+                else
+                {
+                    int number = input[i] - '1' + 1;
+                    output[i] = number;
+                }
+            }
+
+                    // Display the converted array
+            //for (int i = 0; i < output.Length; i++)
+            //{
+            //    Console.Write(output[i] + " ");
+            //}
+            //Console.WriteLine();
+            return output;
+        }
+
+                // Calculate the position of the car number
+        static int Multiplication(int[] plate)
+        {
+            Array.Reverse(plate);
+            double result = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                result += plate[i] * Math.Pow(10, i);
+            }
+            for (int i = 4; i < 7; i++)
+            {
+                result += plate[i] * Math.Pow(10, 3) * Math.Pow(26, i - 3);
+            }
+            return Convert.ToInt32(result);
+        }
+    }
+}
---
+++ Car_Plates/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Car_Plates")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Car_Plates")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bda05923-4084-4a8d-a36b-2eb612293260")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
